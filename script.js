ffce9f3ee5bc0a6ef50411fb76d99c851b11f1e4
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-06-01 04:55:03 +0200

    update

--- script.js
+++ script.js
@@ -3,39 +3,121 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+// load images
 const images = {};
 images.player = new Image();
-images.player.src = "character.png";
-const characterActions = [
-  "up",
-  "top right",
-  "right",
-  "down right",
-  "down",
-  "jump",
-];
+images.player.src = "https://i.ibb.co/Ybk7y06/character.png";
+const characterActions = ["up", "top right", "right", "down right", "down"];
+const numberOfCharacters = 25;
 const characters = [];
-characters.push(new Character());
 
 class Character {
   constructor() {
-    this.width = 103.0625;
-    this.height = 113.125;
+    this.width = 40;
+    this.height = 43.875;
     this.frameX = 3;
-    this.frameY = 3;
-    this.x = 0;
-    this.y = 0;
-    this.speed = Math.random() * 1.5 + 3.5;
+    this.x = Math.random() * canvas.width - this.width;
+    this.y = Math.random() * canvas.height - this.height;
+    this.speed = Math.random() * 2 + 3;
+    this.minFrame = 0;
+    this.action =
+      characterActions[Math.floor(Math.random() * characterActions.length)];
+    if (this.action === "up") {
+      this.frameY = 0;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    } else if (this.action === "top right") {
+      this.frameY = 1;
+      this.minFrame = 4;
+      this.maxFrame = 14;
+    } else if (this.action === "right") {
+      this.frameY = 3;
+      this.minFrame = 3;
+      this.maxFrame = 13;
+    } else if (this.action === "down right") {
+      this.frameY = 4;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    } else if (this.action === "down") {
+      this.minFrame = 0;
+      this.frameY = 6;
+      this.maxFrame = 12;
+    } else if (this.action === "jump") {
+      this.minFrame = 0;
+      this.frameY = 7;
+      this.maxFrame = 9;
+    }
+  }
+  draw() {
+    drawSprite(
+      images.player,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width * 1.5,
+      this.height * 1.5
+    );
+
+    if (this.frameX < this.maxFrame) this.frameX++;
+    else this.frameX = this.minFrame;
+  }
+  update() {
+    if (this.action === "up") {
+      if (this.y < 0 - this.height * 5) {
+        this.y = canvas.height + this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = Math.random() * 2 + 3;
+      } else {
+        this.y -= this.speed;
+      }
+    } else if (this.action === "top right") {
+      if (this.y < 0 - this.height && this.x > canvas.width + this.width) {
+        this.y = canvas.height + this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = Math.random() * 2 + 3;
+      } else {
+        this.y -= this.speed;
+        this.x += this.speed;
+      }
+    } else if (this.action === "right") {
+      if (this.x > canvas.width + this.width * 5) {
+        this.x = 0 - this.width;
+        this.y = Math.random() * canvas.height;
+        this.speed = Math.random() * 2 + 3;
+      } else {
+        this.x += this.speed;
+      }
+    } else if (this.action === "down right") {
+      if (
+        this.y > canvas.height + this.height &&
+        this.x > canvas.width + this.width
+      ) {
+        this.y = 0 - this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = Math.random() * 2 + 3;
+      } else {
+        this.y += this.speed;
+        this.x += this.speed;
+      }
+    } else if (this.action === "down") {
+      if (this.y > canvas.height + this.height * 5) {
+        this.y = 0 - this.height;
+        this.x = Math.random() * canvas.width;
+        this.speed = Math.random() * 2 + 3;
+      } else {
+        this.y += this.speed;
+      }
+    } else if (this.action === "jump") {
+    }
   }
 }
 
-// const playerWidth = 103.0625;
-// const playerHeight = 113.125;
-// let playerFrameX = 3;
-// let playerFrameY = 3;
-// let playerX = 0;
-// let playerY = 0;
-// const playerSpeed = 6;
+for (i = 0; i < numberOfCharacters; i++) {
+  characters.push(new Character());
+}
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
@@ -43,26 +125,13 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSprite(
-    images.player,
-    playerWidth * playerFrameX,
-    playerHeight * playerFrameY,
-    playerWidth,
-    playerHeight,
-    playerX,
-    playerY,
-    playerWidth,
-    playerHeight
-  );
-  // animate sprites
-  if (playerFrameX < 13) playerFrameX++;
-  else playerFrameX = 3;
-  // move player
-  if (playerX < canvas.width + playerWidth) playerX += playerSpeed;
-  else playerX = 0 - playerWidth;
+  for (i = 0; i < characters.length; i++) {
+    characters[i].draw();
+    characters[i].update();
+  }
 }
 
-window.onload = setInterval(animate, 1000 / 30);
+window.onload = setInterval(animate, 1000 / 20);
 
 window.addEventListener("resize", function () {
   canvas.height = window.innerHeight;
